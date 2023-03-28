c36498dbd60886ce8c4b5fda2c591e445ebe24fc
Leah Seyoum <leahseyoum@gmail.com>
2023-03-28 14:43:51 -0700

    set up view

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js")
+const Game = require("../ttt_node/game")
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const game = new Game();
+  const ttt = document.getElementsByClassName("ttt")[0];
+  const view = new View(game, ttt);
+ 
 });
--- src/ttt-view.js
+++ src/ttt-view.js
@@ -1,7 +1,24 @@
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
+    this.game = game;
+    this.el = el;
+    this.setupBoard();
+  }
 
-  setupBoard() {}
+  setupBoard() {
+    this.grid = [];
+    for(let i = 0; i < 3; i++) {
+      let row = [];
+      for(let j = 0; j < 3; j++) {
+        let li = document.createElement("li");
+        li.textContent = "hello";
+        this.el.appendChild(li);
+        row.push(li)
+      }
+      this.grid.push(row);
+    }
+  }
+
   
   bindEvents() {}
 
